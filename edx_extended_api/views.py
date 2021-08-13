46dbe675717586c54a6fb23ed5ecdd7f276ccc21
Maxim Starodubcev <maxim.starodubcev@raccoongang.com>
2021-08-13 17:01:13 +0300

    Add get_user API

--- edx_extended_api/views.py
+++ edx_extended_api/views.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, ListModelMixin
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from openedx.core.lib.api.authentication import OAuth2AuthenticationAllowInactiveUser
-from serializers import UserSerializer
+from serializers import UserSerializer, ListUserSerializer
 from django.contrib.auth import get_user_model
 
 
@@ -23,3 +23,23 @@ class CreateUserView(CreateModelMixin, UpdateModelMixin, viewsets.GenericViewSet
 
     def update(self, request, *args, **kwargs):
         return super(CreateUserView, self).update(request, *args, **kwargs)
+
+
+class ListUserView(ListModelMixin, viewsets.GenericViewSet):
+    authentication_classes = (OAuth2AuthenticationAllowInactiveUser,)
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ListUserSerializer
+
+    def get_queryset(self):
+        """
+        Restricts the returned users, by filtering by `username` or `user_id` query parameter.
+        """
+        queryset = User.objects.all()
+        usernames = [u.strip() for u in self.request.query_params.get('username', '').split(',') if u.strip()]
+        user_ids = [int(_id) for _id in self.request.query_params.get('user_id', '').split(',') if _id.strip().isdigit()]
+
+        if usernames:
+            queryset = queryset.filter(username__in=usernames)
+        elif user_ids:
+            queryset = queryset.filter(pk__in=user_ids)
+        return queryset
