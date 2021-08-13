b4ede57a93444a4d16a65aa41ef28f75e95a243f
Vladyslav Zherebkin <vladyslav.zherebkin@raccoongang.com>
2021-08-13 15:12:46 +0300

    feat: [LTEF-34] Implemented single user update

--- edx_extended_api/views.py
+++ edx_extended_api/views.py
@@ -2,18 +2,24 @@
 from __future__ import unicode_literals
 
 from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from openedx.core.lib.api.authentication import OAuth2AuthenticationAllowInactiveUser
-
 from serializers import UserSerializer
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class CreateUserView(CreateModelMixin, UpdateModelMixin, viewsets.GenericViewSet):
     authentication_classes = (OAuth2AuthenticationAllowInactiveUser,)
     permission_classes = (IsAuthenticated,)
-
+    queryset = User.objects.all()
     serializer_class = UserSerializer
 
     def create(self, request, *args, **kwargs):
         return super(CreateUserView, self).create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        return super(CreateUserView, self).update(request, *args, **kwargs)
